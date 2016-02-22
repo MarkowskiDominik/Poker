3bb7f21ab9bb08cd4378475142bdc9ef14d7c302
Tomasz Szala <tomasz.szala@gmail.com>
2016-02-22 07:28:39 +0100

    Code review performed

--- src/main/java/Poker/Poker.java
+++ src/main/java/Poker/Poker.java
@@ -1,4 +1,4 @@
-package Poker;
+package main.java.Poker;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -9,43 +9,50 @@ import java.util.logging.Logger;
 
 public class Poker {
 
-	Parser parser;
-	BufferedReader fileReader;
-	Logger logger = Logger.getLogger("Poker");
-	
-	public Poker(String fileName) {
-		openFileToRead(fileName);
-		parser = new Parser();
-	}
-	
-	private void openFileToRead(String fileName) {
-		try {
-			fileReader = new BufferedReader(new FileReader(fileName));
-		} catch (FileNotFoundException e) {
-			logger.info("file not found");
-		}
-	}
-	
-	private void closeFile() throws IOException {
-		fileReader.close();
-	}
-	
-	public Integer numberOfWinsFirstPlayer() throws IOException {
-		Integer numberOfWin = 0;
-		List<Hand> playersHands = null;
-		
-		while (fileReader.ready()) {
-			try {
-				playersHands = parser.parserRound(fileReader.readLine());
-			} catch (Exception e) {
-				logger.info("invalid structure in file");
-			}
-			if (playersHands.get(0).compareTo(playersHands.get(1)) == 1) {
-				numberOfWin++;
-			}
-		}
-		closeFile();
-		logger.info(numberOfWin.toString());
-		return numberOfWin;
-	}
+    // REVIEW dmarkowski - missing private modifiers for the variables, additionally all which doesn't change should be
+    // final
+    Parser parser;
+    BufferedReader fileReader;
+    Logger logger = Logger.getLogger("Poker");
+
+    public Poker(String fileName) {
+        openFileToRead(fileName);
+        parser = new Parser();
+    }
+
+    private void openFileToRead(String fileName) {
+        try {
+            fileReader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            logger.info("file not found");
+        }
+    }
+
+    private void closeFile() throws IOException {
+        fileReader.close();
+    }
+
+    public Integer numberOfWinsFirstPlayer() throws IOException {
+        Integer numberOfWin = 0;
+        List<Hand> playersHands = null;
+
+        // REVIEW dmarkowski - Please separate read and business logic, since the input data is not big it can be read
+        // as whole by the program. Currently reading the input and processing is mixed
+        while (fileReader.ready()) {
+            try {
+                playersHands = parser.parserRound(fileReader.readLine());
+            } catch (Exception e) {
+                // REVIEW dmarkowski - to general catch clause, it will also catch all programmers errors, which are not
+                // connected with the file conditions
+                logger.info("invalid structure in file");
+            }
+            // REVIEW dmarkowski - pottential NPE when the parser throws an exception, then the playersHands is null
+            if (playersHands.get(0).compareTo(playersHands.get(1)) == 1) {
+                numberOfWin++;
+            }
+        }
+        closeFile();
+        logger.info(numberOfWin.toString());
+        return numberOfWin;
+    }
 }
--- src/test/java/Poker/ParserTest.java
+++ src/test/java/Poker/ParserTest.java
@@ -1,60 +1,62 @@
-package Poker;
+package test.java.Poker;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.java.Poker.Parser;
+
 public class ParserTest {
 
-	Parser parser;
-	
-	@Before
-	public void initializeParser() {
-		parser = new Parser();
-	}
-	
-	@After
-	public void finalizeParser() {
-		parser = null;
-	}
-	
-	@Test(expected = Exception.class)
-	public void toManyCards() {
-		//given
-		String round = "KC QC AC JC TC 7D 2S 5D 3S AC AC";
-		
-		//when		
-		//then
-		parser.parserRound(round);
-	}
-	
-	@Test(expected = Exception.class)
-	public void noTeenCards() {
-		//given
-		String round = "KC QC AC JC TC 7D 2S 5D 3S";
-		
-		//when		
-		//then
-		parser.parserRound(round);
-	}
+    Parser parser;
 
-	@Test(expected = Exception.class)
-	public void invalidCardRankValue() {
-		//given
-		String round = "ZC ZC AC JC TC 7D 2S 5D 3S AC";
-		
-		//when		
-		//then
-		parser.parserRound(round);
-	}
-	
-	@Test(expected = Exception.class)
-	public void invalidCardSuitValue() {
-		//given
-		String round = "KZ QZ AC JC TC 7D 2S 5D 3S AC";
-		
-		//when		
-		//then
-		parser.parserRound(round);
-	}
+    @Before
+    public void initializeParser() {
+        parser = new Parser();
+    }
+
+    @After
+    public void finalizeParser() {
+        parser = null;
+    }
+
+    @Test(expected = Exception.class)
+    public void toManyCards() {
+        // given
+        String round = "KC QC AC JC TC 7D 2S 5D 3S AC AC";
+
+        // when
+        // then
+        parser.parserRound(round);
+    }
+
+    @Test(expected = Exception.class)
+    public void noTeenCards() {
+        // given
+        String round = "KC QC AC JC TC 7D 2S 5D 3S";
+
+        // when
+        // then
+        parser.parserRound(round);
+    }
+
+    @Test(expected = Exception.class)
+    public void invalidCardRankValue() {
+        // given
+        String round = "ZC ZC AC JC TC 7D 2S 5D 3S AC";
+
+        // when
+        // then
+        parser.parserRound(round);
+    }
+
+    @Test(expected = Exception.class)
+    public void invalidCardSuitValue() {
+        // given
+        String round = "KZ QZ AC JC TC 7D 2S 5D 3S AC";
+
+        // when
+        // then
+        parser.parserRound(round);
+    }
 }
